ff21d08ed9c5647f37a015cff263a98e09682340
fiqriismail <fiqri.ismail@gmail.com>
2022-12-29 12:01:42 +0530

    Created students table columns and migrated to the database

--- database/migrations/2022_12_29_062631_create-students-table.php
+++ database/migrations/2022_12_29_062631_create-students-table.php
@@ -14,6 +14,14 @@ return new class extends Migration
     public function up()
     {
         //
+        Schema::create("students", function (Blueprint $table)
+        {
+            $table->uuid("id");
+            $table->string("first_name", 250);
+            $table->string("last_name", 250);
+            $table->date("joined_date");
+            $table->timestamps();
+        });
     }
 
     /**
