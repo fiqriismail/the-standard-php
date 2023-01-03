7b06c37319862571d1360fda438fcd9c35e20d72
fiqriismail <fiqri.ismail@gmail.com>
2023-01-03 14:31:55 +0530

    Created students factory

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -17,7 +17,8 @@ class StudentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            "first_name" => $this->faker->firstName,
+            "last_name" => $this->faker->lastName,
         ];
     }
 }
